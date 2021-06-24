608d40aaa51a85de8053f3ac6287a822f7e897f7
jibundeyare <contact@jibundeyare.com>
2021-06-24 23:42:13 +0200

    Amélioration des commentaires

--- mail-mta.php
+++ mail-mta.php
@@ -9,15 +9,15 @@ $transport = new Swift_SendmailTransport('/usr/sbin/sendmail');
 $mailer = new Swift_Mailer($transport);
 
 // Choix du sujet du mail
-$message = (new Swift_Message('Hello Swiftmailer SMTP!'))
+$message = (new Swift_Message('Hello Swiftmailer MTA!'))
     // Choix de l'adresse de l'expéditeur
-    // Même si mettez un from différent du username, l'adresse du
-    // username sera visible dans l'entête du mail
+    // Même si mettez une adresse "from" différent du compte SMTP,
+    // l'adresse du compte SMTP sera visible dans l'entête du mail
     ->setFrom(['foo.bar@example.com' => 'Foo Bar'])
     // Choix du destinataire
     ->setTo(['bar.baz@example.com'])
     // Choix du message
-    ->setBody('Hello Swiftmailer SMTP!')
+    ->setBody('Hello Swiftmailer MTA!')
 ;
 
 // Envoi du message
