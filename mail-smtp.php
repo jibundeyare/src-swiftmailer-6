57c1e9580710fe60d3ff60beeb64631e05a1d32c
jibundeyare <contact@jibundeyare.com>
2021-06-24 23:41:47 +0200

    Amélioration des commentaires

--- mail-smtp.php
+++ mail-smtp.php
@@ -18,8 +18,8 @@ $mailer = new Swift_Mailer($transport);
 // Choix du sujet du mail
 $message = (new Swift_Message('Hello Swiftmailer SMTP!'))
     // Choix de l'adresse de l'expéditeur
-    // Même si mettez un from différent du username, l'adresse du
-    // username sera visible dans l'entête du mail
+    // Même si mettez une adresse "from" différent du compte SMTP,
+    // l'adresse du compte SMTP sera visible dans l'entête du mail
     ->setFrom(['foo.bar@example.com' => 'Foo Bar'])
     // Choix du destinataire
     ->setTo(['bar.baz@example.com'])
